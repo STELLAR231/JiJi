eb74fcad785b61610e3210e176e6e5324bd72fb4
STELLAR231 <boypoke03@gmail.com>
2026-02-05 11:32:46 +0000

    Add renderChart function for chart visualization

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,9 @@
-// Charts module
+window.renderChart = function(container) {
+  container.innerHTML = `
+    <div style="margin-top:24px">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  `;
+};
